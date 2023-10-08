5f1175e7feec78e955465e8d85eec43ea6cd3296
aqswa <joayoung3@gmail.com>
2023-10-08 18:03:15 +0900

    Initial Commit

---
+++ src/main/java/efub/ebmt/eeojum/EeojumApplication.java
@@ -0,0 +1,13 @@
+package efub.ebmt.eeojum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeojumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeojumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efub/ebmt/eeojum/EeojumApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.ebmt.eeojum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeojumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
