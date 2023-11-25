1248cb676dab25d4587eb551e754b58cf35ed36f
aqswa <joayoung3@gmail.com>
2023-11-26 02:18:40 +0900

    fix: 보육 조회 get -> post

--- src/main/java/efub/ebmt/eeojum/domain/nurture/controller/NurtureController.java
+++ src/main/java/efub/ebmt/eeojum/domain/nurture/controller/NurtureController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/nurtures")
@@ -20,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NurtureController {
     private final NurtureService nurtureService;
 
-    @GetMapping
+    @PostMapping
     @Operation(summary = "보육 시설 정보 조회")
     public ResponseEntity<NurturesResponse> getNurtureList(@RequestBody NurtureRequest nurtureRequest){
         return new ResponseEntity<>(nurtureService.nurtureList(nurtureRequest), HttpStatus.OK);
